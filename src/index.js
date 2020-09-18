3db11ca6f95f54c4d4a7ef7e608345fd2d980811
xu3cjo4vup <lee_huihsin@hotmail.com>
2020-09-18 16:01:50 +0800

    add title and search field

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
-import { robots } from './robots.js';
 
-ReactDOM.render(
-              <div>
-                <CardList robots={robots}/>
-              </div>
-  , document.getElementById('root'));
+
+ReactDOM.render(<div><App /></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
