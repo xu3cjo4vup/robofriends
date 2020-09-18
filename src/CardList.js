7a6545184b5d995fe10a042251fd3a2386b90cfb
xu3cjo4vup <lee_huihsin@hotmail.com>
2020-09-18 14:14:40 +0800

    add cardlist component

---
+++ src/CardList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card.js';
+
+const CardList = ({robots}) => {
+    
+    return (
+        <div>
+            
+            {
+            robots.map((user,i) => {
+            return (
+                <Card 
+                key={i} 
+                name={robots[i].name} 
+                id={robots[i].id} 
+                email={robots[i].email}
+                />
+                )
+            })
+            }
+        </div>
+    )
+}
+
+export default CardList;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { robots } from './robots.js';
 
 ReactDOM.render(
               <div>
-                <Card name={robots[0].name} id={robots[0].id} email={robots[0].email} />
-                <Card name={robots[1].name} id={robots[1].id} email={robots[1].email} />
-                <Card name={robots[2].name} id={robots[2].id} email={robots[2].email} />
+                <CardList robots={robots}/>
               </div>
   , document.getElementById('root'));
 
