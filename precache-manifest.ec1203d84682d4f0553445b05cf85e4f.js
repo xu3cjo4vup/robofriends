3af3c32a755d0286169df3cbc3893f2cbe3a2c19
xu3cjo4vup <lee_huihsin@hotmail.com>
2020-09-19 20:43:45 +0800

    Updates

---
+++ precache-manifest.ec1203d84682d4f0553445b05cf85e4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c886fefee05cb12ce43b86aa59f7500",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9bef4998d522793f9f26",
+    "url": "/robofriends/static/css/main.5e06bbf0.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bef4998d522793f9f26",
+    "url": "/robofriends/static/js/main.2a1f1772.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
